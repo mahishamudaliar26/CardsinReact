10faf9a1de0b9dba483ac2ac45649b68111cc99c
mahisha-mudaliar <mahishamudaliar2603@gmail.com>
2024-01-03 23:44:40 +0530

    First

---
+++ CardsinReact/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Badge from './components/Badge'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='App'>
+        <Badge
+        text="New Post"/>
+      </div>
+    </>
+  )
+}
+
+export default App
